bdf12786e16d172d9925a3bfda22d195273ea382
mahsanfurqan <ahsanfurkab@gmail.com>
2026-01-21 16:21:22 +0700

    Build web and update Netlify config

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -37,7 +37,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "f8fc562902ec2869387c1535b8063ca2
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "907397a848ba55f28731d1b23befb2c1",
+"flutter_bootstrap.js": "2eeeac9646a7aac0cddae69dca7a945b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
